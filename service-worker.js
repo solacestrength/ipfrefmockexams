d0216e27e4aa24b357a8fad8960a4780e4612753
solacestrength <solacestrengthcoaching@gmail.com>
2025-11-30 19:23:36 +0000

    Add service worker for caching resources

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "ipf-exam-cache-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/css/style.css",
+  "/js/app.js",
+  "/js/exams.js",
+  "/js/exam1.js",
+  "/js/exam2.js",
+  "/js/exam3.js",
+  "/js/exam4.js",
+  "/js/exam5.js",
+  "/js/exam6.js",
+  "/js/exam7.js",
+  // add icons if you have them
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
